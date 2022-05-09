7b555e16c0cfce7812ccf3a9009110ed1cf19c71
x <karolina_bp@hotmail.com>
2022-05-09 17:03:27 -0300

    Calculo de imposto tipo ICC

---
+++ CursoDesignPatterns/CursoDesignPatterns/ICCC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class ICCC : Imposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if(orcamento.Valor < 1000.0)
+            {
+                return orcamento.Valor * 0.5;
+            }
+            else if(orcamento.Valor >= 1000.0 && orcamento.Valor <= 3000.0)
+            {
+                return orcamento.Valor * 0.7;
+            }
+            else
+            {
+                return (orcamento.Valor * 0.8) + 30.0;
+            }
+        }
+    }
+}
--- CursoDesignPatterns/CursoDesignPatterns/Program.cs
+++ CursoDesignPatterns/CursoDesignPatterns/Program.cs
@@ -8,12 +8,14 @@ namespace CursoDesignPatterns
         {
             Imposto iss = new ISS();
             Imposto icms = new ICMS();
+            Imposto iccc = new ICCC();
 
             Orcamento orcamento = new Orcamento(500.0);
 
             CalculadorDeImpostos calculador = new CalculadorDeImpostos();
             calculador.RealizaCalculo(orcamento, icms);
             calculador.RealizaCalculo(orcamento, iss);
+            calculador.RealizaCalculo(orcamento, iccc);
 
             Console.ReadKey();
         }
